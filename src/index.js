ba58c02e6075acb8fa08ec4c78d7c1a61eaee7da
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-24 00:48:06 +1000

    step three complete

--- src/index.js
+++ src/index.js
@@ -2,19 +2,47 @@ document.addEventListener('DOMContentLoaded', function(){
 
 const URL = 'http://localhost:3000/pups'
 const dogBar = document.getElementById('dog-bar')
+const dogInfoDiv = document.getElementById('dog-info')
 
-let eachDogName, dogBarInnerSpan
+let eachDog, eachDogName, dogBarInnerSpan, dogInfoImg, dogInfoTitle, dogInfoGOrB, dogInfo
+let isClicked = false
 
 fetch(URL)
 .then(resp=>resp.json())
 .then((data)=>{addToDogBar(data)})
 
 function addToDogBar(obj){
-    eachDogName = Object.values(obj)
-    for(i=0;i<eachDogName.length;i++){
+    eachDog = Object.values(obj)
+    for(let i=0;i<eachDog.length;i++){
+        eachDogName = eachDog[i].name
         dogBarInnerSpan = dogBar.appendChild(document.createElement('span'))
-        dogBarInnerSpan.innerHTML = eachDogName[i].name
-        console.log(dogBarInnerSpan)
+        dogBarInnerSpan.innerHTML = eachDogName
+        dogBarInnerSpan.style.cursor = 'pointer'
+        dogBarInnerSpan.addEventListener('click', function(){openDogInfo(eachDog[i].name, eachDog[i].image, eachDog[i].isGoodDog)})
+        console.log(eachDogName)
+    }
+}
+
+function openDogInfo(name, image, isGood){
+    if(isClicked){
+        dogInfo = document.querySelectorAll('.dogInfo')
+        for(i=0;i<dogInfo.length;i++){
+            dogInfo[i].remove()
+        }
+    }
+    isClicked = true
+    dogInfoImg = dogInfoDiv.appendChild(document.createElement('img'))
+    dogInfoImg.setAttribute('src', image)
+    dogInfoImg.setAttribute('class', 'dogInfo')
+    dogInfoTitle = dogInfoDiv.appendChild(document.createElement('h2'))
+    dogInfoTitle.setAttribute('class', 'dogInfo')
+    dogInfoTitle.innerHTML = name
+    dogInfoGOrB = dogInfoDiv.appendChild(document.createElement('button'))
+    dogInfoGOrB.setAttribute('class', 'dogInfo')
+    if(isGood){
+        dogInfoGOrB.innerHTML = 'Good doggo'
+    } else {
+        dogInfoGOrB.innerHTML = 'Bad doggo'
     }
 }
 
